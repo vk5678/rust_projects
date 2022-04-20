129c4d29cf93b2e97f2123454fa3e0ed74af82ee
Manikanta <manikantavinodkumar888@gmail.com>
2022-04-21 01:19:29 +0530

    changed the code to be readable and approachable methods

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,9 @@ use std::{
 };
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    let mut string: String = String::new();
+    File::open("hello.txt")?.read_to_string(&mut string)?;
+    Ok(string)
 }
 fn main() {
     println!("{:?}", read_username_from_file());
